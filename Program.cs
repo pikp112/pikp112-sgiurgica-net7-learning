e0d93f7e637ad756fd2348368893e6474e55ce4a
SorinG <sorinnnn96@yahoo.com>
2022-11-14 22:41:38 +0200

    Update Program.cs with c# homework

--- Program.cs
+++ Program.cs
@@ -110,3 +110,95 @@
     }
 
         //Exercise 7
+    System.Console.Write("Enter a month number from 1 to 12: ");
+    byte monthNumber=Convert.ToByte(System.Console.ReadLine());
+    if (monthNumber>=1 && monthNumber<=12){
+    switch(monthNumber){
+        case 1:
+            System.Console.WriteLine("January");
+            break;
+        case 2:   
+            System.Console.WriteLine("February");
+            break;
+        case 3:
+            System.Console.WriteLine("March");
+            break;
+        case 4:   
+            System.Console.WriteLine("April");
+            break;
+        case 5:
+            System.Console.WriteLine("May");
+            break;
+        case 6:   
+            System.Console.WriteLine("June");
+            break;
+        case 7:
+            System.Console.WriteLine("July");
+            break;
+        case 8:   
+            System.Console.WriteLine("August");
+            break;
+        case 9:   
+            System.Console.WriteLine("September");
+            break;        
+        case 10:   
+            System.Console.WriteLine("Octomber");
+            break;        
+        case 11:   
+            System.Console.WriteLine("November");
+            break;        
+        case 12:   
+            System.Console.WriteLine("December");
+            break;
+    }
+    }
+    else {
+        System.Console.WriteLine("You entered a wrong number. Enter again a number from 1 to 12.");
+    }
+
+        //Exercise 8
+    System.Console.Write("Enter the temperature in centigrade from your city: ");
+    int temperature=Convert.ToInt16(System.Console.ReadLine());
+    if(temperature>=-50 && temperature<=50){
+    if(temperature<0){
+        System.Console.WriteLine("Freezing weather");
+    }
+    else if(temperature >=0 && temperature <=10){
+        System.Console.WriteLine("Very Cold weather");
+    }
+    else if(temperature >=10 && temperature <=20){
+        System.Console.WriteLine("Cold weather");
+    }
+    else if(temperature >=20 && temperature <=30){
+        System.Console.WriteLine("Normal in Temp");
+    }
+    else if(temperature >=30 && temperature <=40){
+        System.Console.WriteLine("Its hot");
+    }
+    else if(temperature >= 40){
+        System.Console.WriteLine("Its very hot");
+        }
+    } else {
+        System.Console.WriteLine("The temperature entered is extreme! You probably entered it wrong");
+    }  
+
+        //Exercise 9
+
+    System.Console.WriteLine("Introduceti primul numar:");
+    double num1=Convert.ToDouble(System.Console.ReadLine());
+    System.Console.WriteLine("Introduceti al doilea numar:");
+    double num2=Convert.ToDouble(System.Console.ReadLine());
+    double addition=num1+num2;
+    System.Console.WriteLine("Addition: {0}.\n",addition);
+    System.Console.WriteLine("Decrese: num1 - num2 = "+ (num1-num2)+".\n");
+    double multiplication=num1*num2;
+    System.Console.WriteLine("Multiplication: " + multiplication+".\n");
+    System.Console.WriteLine("Division: "+(num1/num2)+".\n");
+    double modulus=num1%num2;
+    System.Console.WriteLine("Modulus: {0}.\n", modulus);
+    num1++;num2++;
+    System.Console.WriteLine("Incrementation of first number like num1++ is "+num1+" and of second number like num2++ is "+num2+".\n");
+    num1--;num2--;//operatiune pentru a reveni la valorile initiale ale lui num1 si num2
+    System.Console.WriteLine("Short form of num1=num1+num2 is num1+=num2 and the result is "+ (num1+=num2) +".\n");
+    num1-=num2;//operatiune pentru a reveni la valorile initiale ale lui num1 si num2
+    System.Console.WriteLine("Short form of num1=num1-num2 is num1-=num2 and the result is "+ (num1-=num2) +".\n");
