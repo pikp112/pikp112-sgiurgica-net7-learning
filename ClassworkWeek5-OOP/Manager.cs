f34902683997b14c938c72648957e6972d2c0b52
pikp <sorinnnn96@yahoo.com>
2022-12-20 08:03:08 +0200

    ClassWork Week5-OOP
    I did it before, I realized later that it's not the homework, sorry

---
+++ ClassworkWeek5-OOP/Manager.cs
@@ -0,0 +1,40 @@
+﻿namespace Week5_OOP_Advanced
+{
+    sealed class Manager : Employee
+    {
+        public List<Employee> Employee = new List<Employee>();
+
+        public Manager(string firstName, string lastName, double salary)
+        {
+            FirstName= firstName;
+            LastName= lastName;
+            Salary= salary;
+        }
+        public void AddEmployee(RegularEmployee employee)
+        {
+            Employee.Add(employee);
+        }
+
+        public double GetTotalMonthlySalary()
+        {
+            double totalMonthlySalary = 0;
+            foreach (var employee in Employee)
+            {
+               totalMonthlySalary+=employee.GetMonthlySalary();
+            }
+            return totalMonthlySalary+this.GetMonthlySalary();
+            
+
+        }
+        public override string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public override double GetMonthlySalary()
+        {
+            return Salary/12;
+        }
+
+    }
+}
