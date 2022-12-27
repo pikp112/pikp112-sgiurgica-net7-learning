f8a3f719b814f9463114787db5992f6bc77e0aff
Sorin <85454099+pikp112@users.noreply.github.com>
2022-12-27 16:24:00 +0200

    Update Faculty.cs

--- Faculty.cs
+++ Faculty.cs
@@ -23,10 +23,7 @@
 
         public void GetSubjectsTaught()
         {
-            foreach(string elem in _subjectsTaught)
-            {
-                Console.WriteLine(elem);
-            }
+            return _subjectsTaught;
         }
 
 
