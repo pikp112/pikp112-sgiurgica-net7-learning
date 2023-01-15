c12eeab3bfc6c3a34031a42d720a8e8cedc4004f
pikp <sorinnnn96@yahoo.com>
2023-01-15 13:03:06 +0200

    Update homework week 07

---
+++ HomeWorkWeek07_ExceptionsExtensionLinq/HomeWorkWeek07_ExceptionsExtensionLinq/Program.cs
@@ -0,0 +1,41 @@
+﻿using HomeWorkWeek07_ExceptionsExtensionLinq;
+using HomeWorkWeek07_ExceptionsExtensionLinq.Ex1;
+using HomeWorkWeek07_ExceptionsExtensionLinq.Ex10;
+using HomeWorkWeek07_ExceptionsExtensionLinq.Ex2;
+using HomeWorkWeek07_ExceptionsExtensionLinq.Ex3;
+using HomeWorkWeek07_ExceptionsExtensionLinq.Ex4;
+using HomeWorkWeek07_ExceptionsExtensionLinq.Ex6;
+using HomeWorkWeek07_ExceptionsExtensionLinq.Ex8;
+
+//Ex1
+//CheckPastOrFuture.Start();
+
+//Ex2
+//CheckNamesList.Start();
+
+//Ex3
+//CheckMathOp.Start();
+
+//Ex4
+CheckFile.Start();
+
+//Ex5
+//ExtensionMethodDateTimeToString.Start();
+
+//Ex6
+//IntToBinary.Start();
+
+//Ex7
+//SumSquaresEvenNum.Start();
+
+//Ex8
+//NewListSquareNum.Start();
+
+//Ex9
+//OrderStudentByGrade.Start();
+
+//Ex10
+//SortProducts.Start();
+
+//Ex11
+//
